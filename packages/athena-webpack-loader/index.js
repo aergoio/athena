219c98e0b4394bd3e562a61df6550d3767f3d6fe
Taeik Lim <sibera21@gmail.com>
2020-06-22 21:18:19 +0900

    [#2] Update athena compiler version

--- packages/athena-webpack-loader/index.js
+++ packages/athena-webpack-loader/index.js
@@ -5,8 +5,7 @@ module.exports = async function athenaLoader(source) {
   const filePath = loaderContext.resource;
   const luaCompiler = new CompilerFactory().create(CompilerType.Lua);
   const compileResult = await luaCompiler.compile(source, filePath);
-  // Clean up ABI and Payload
-  compileResult.abi = JSON.parse(compileResult.abi);
-  compileResult.payload = compileResult.payload.trim();
+  compileResult.abi = compileResult.abi;
+  compileResult.payload = compileResult.payload;
   return `module.exports = ${JSON.stringify(compileResult)};`;
 };
